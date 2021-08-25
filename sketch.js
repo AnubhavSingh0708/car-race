128ad3e4c2d9e680d9f1a162f3d55cf68b50e917
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-08-25 09:50:42 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,16 +4,14 @@ var h = window.innerHeight;
 function windowPrint(){
   window.print();
 }
+setTimeout(function(){
+rotate();
+}, 5000);
 function goRed(){
   redY=redY+1.5;
   document.getElementById("red").style.bottom = redY+"%";
 }
 function run(){
-setTimeout(function(){
-  check();
-  blueY=blueY+0.49;
-   document.getElementById("blue").style.bottom = blueY+"%";
-}, 30);
 }
 function check(){
    h = window.innerHeight;
